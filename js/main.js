19a9cf28216e99e8da6f8a4996d5753bfdb948d2
Rute-r <rute_ke@hotmail.com>
2023-06-26 11:05:29 -0300

    Implementação da função deletaElemento

--- js/main.js
+++ js/main.js
@@ -2,8 +2,6 @@ const form = document.getElementById('novoItem');
 const lista = document.getElementById('lista');
 const itens = JSON.parse(localStorage.getItem('itens')) || [];
 
-console.log(itens);
-
 itens.forEach((elemento) => {
   criaElemento(elemento);
 });
@@ -23,9 +21,9 @@ form.addEventListener('submit', (evento) => {
     itemAtual.id = existe.id;
 
     atualizaELemento(itemAtual);
-    itens[existe.id] = itemAtual;
+    itens[itens.findIndex((elemento) => elemento.id === existe.id)] = itemAtual;
   } else {
-    itemAtual.id = itens.length;
+    itemAtual.id = itens[itens.length - 1] ? itens[itens.length - 1].id + 1 : 0;
 
     criaElemento(itemAtual);
     itens.push(itemAtual);
@@ -46,10 +44,34 @@ function criaElemento(item) {
   numeroItem.dataset.id = item.id;
   novoItem.appendChild(numeroItem);
   novoItem.innerHTML += item.nome;
+  novoItem.appendChild(botaoDeleta(item.id));
 
   lista.appendChild(novoItem);
 }
 
 function atualizaELemento(item) {
   document.querySelector("[data-id='" + item.id + "']").innerHTML = item.quantidade;
+}
+
+function botaoDeleta(id) {
+  const elementoBotao = document.createElement('button');
+
+  elementoBotao.innerText = 'X';
+
+  elementoBotao.addEventListener('click', function () {
+    deletaElemento(this.parentNode, id);
+  });
+
+  return elementoBotao;
+}
+
+function deletaElemento(tag, id) {
+  tag.remove();
+
+  itens.splice(
+    itens.findIndex((elemento) => elemento.id === id),
+    1
+  );
+
+  localStorage.setItem('itens', JSON.stringify(itens));
 }
